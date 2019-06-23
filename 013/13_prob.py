61f3c74fbf9bdceb41c4e848fd7ddf23588b97a5
ismail <is.abdelwahab@gmail.com>
2019-06-23 18:41:38 +0200

    13'th pb Project Euler Done in python

---
+++ 013/13_prob.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#----------------------------------------------------------------
+#  Copyright (c) ABDEL WAHAB Ismail.      Created: June 23 2019				
+#   https://github.com/ismailAbdelwahab/Project_Euler_Wrprime_factor_Up	
+#----------------------------------------------------------------
+#----------------------------------------------------------------
+#        13th Problem : https://projecteuler.net/problem=13
+#
+#     10 first digits of the sum of all numbers in bigNumber.txt.
+#----------------------------------------------------------------
+
+
+# Get the numbers in a list:
+with open("bigNumber.txt","r") as f:
+	numbers = f.read().split("\n")
+
+# Convert all string_number into int:
+numbers = [int(i) for i in numbers]
+
+#-------------
+#   OUTPUT	
+#-------------
+print ("  The first 10 digits of the sum of all these numbers are :")
+print("   Sum =  " + str(sum(numbers))[0:10] )
